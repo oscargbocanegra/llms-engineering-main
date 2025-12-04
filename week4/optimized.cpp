c5f2250a2505380bb957c281b14b594350070f9c
oscargiovanni <oscargiovanni@gmail.com>
2025-12-03 22:47:16 -0500

    Update run test

--- week4/optimized.cpp
+++ week4/optimized.cpp
@@ -1,33 +1,29 @@
-```cpp
 #include <iostream>
 #include <iomanip>
 #include <chrono>
 
-double calculate(long long iterations, long long param1, long long param2) {
+double calculate(int64_t iterations, double param1, double param2) {
     double result = 1.0;
-    
-    for (long long i = 1; i <= iterations; ++i) {
-        long long j = i * param1 - param2;
+    for (int64_t i = 1; i <= iterations; ++i) {
+        double j = i * param1 - param2;
         result -= (1.0 / j);
         j = i * param1 + param2;
         result += (1.0 / j);
     }
-    
     return result;
 }
 
 int main() {
     auto start_time = std::chrono::high_resolution_clock::now();
     
-    double result = calculate(200'000'000, 4, 1) * 4;
-    
+    double result = calculate(200000000, 4.0, 1.0) * 4.0;
+
     auto end_time = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = end_time - start_time;
-    
+    std::chrono::duration<double> execution_time = end_time - start_time;
+
     std::cout << std::fixed << std::setprecision(12);
     std::cout << "Result: " << result << std::endl;
-    std::cout << "Execution Time: " << diff.count() << " seconds" << std::endl;
-    
+    std::cout << "Execution Time: " << execution_time.count() << " seconds" << std::endl;
+
     return 0;
 }
-```
